db5b79c41fa9013064193453daf6ec72940d7c5e
Andridge Munene <andridgemunene@MAC.local>
2023-04-13 16:37:57 +0300

    fCK

--- server.js
+++ server.js
@@ -1,40 +1,47 @@
-  // Virtual entry point for the app
-  import * as remixBuild from '@remix-run/server-runtime';
-  import {createStorefrontClient, storefrontRedirect} from '@shopify/hydrogen';
-  import {
-    createRequestHandler,
-    getBuyerIp,
-    createCookieSessionStorage,
-  } from '@shopify/remix-oxygen';
+// Virtual entry point for the app
+import * as remixBuild from '@remix-run/server-runtime';
+import {createStorefrontClient, storefrontRedirect} from '@shopify/hydrogen';
+import {
+  createRequestHandler,
+  getBuyerIp,
+  createCookieSessionStorage,
+} from '@shopify/remix-oxygen';
 
-  /**
-   * Export a fetch handler in module format.
-   */
-   export default async function fetch(request, env, executionContext) {
-    try {
-      // Only use caches if it's available (i.e. in the Service Worker context)
-      const cache = typeof caches !== 'undefined' ? await caches.open('hydrogen') : null;
-  
-      /**
-       * Open a custom session instance.
-       */
-      const session = await HydrogenSession.init(request, [env.SESSION_SECRET]);
-  
-     /**
- * Create Hydrogen's Storefront client.
+/**
+ * Export a fetch handler in module format.
  */
-const {storefront} = createStorefrontClient({
-  cache,
-  waitUntil: (p) => executionContext.waitUntil(p),
-  buyerIp: getBuyerIp(request),
-  i18n: {language: 'EN', country: 'US'},
-  publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-  privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-  storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
-  storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
-  storefrontId: env.PUBLIC_STOREFRONT_ID,
-  requestGroupId: request.headers && request.headers instanceof Headers && request.headers.get('request-id'),
-});
+export default {
+  async fetch(request, env, executionContext) {
+    try {
+      /**
+       * Open a cache instance in the worker and a custom session instance.
+       */
+      if (!env?.SESSION_SECRET) {
+        throw new Error('SESSION_SECRET environment variable is not set');
+      }
+
+      const waitUntil = (p) => executionContext.waitUntil(p);
+      const [cache, session] = await Promise.all([
+        caches.open('hydrogen'),
+        HydrogenSession.init(request, [env.SESSION_SECRET]),
+      ]);
+
+      /**
+       * Create Hydrogen's Storefront client.
+       */
+      const {storefront} = createStorefrontClient({
+        cache,
+        waitUntil,
+        buyerIp: getBuyerIp(request),
+        i18n: {language: 'EN', country: 'US'},
+        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+        storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+        storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
+        storefrontId: env.PUBLIC_STOREFRONT_ID,
+        requestGroupId: request.headers.get('request-id'),
+      });
+
       /**
        * Create a Remix request handler and pass
        * Hydrogen's Storefront client to the loader context.
@@ -44,9 +51,9 @@ const {storefront} = createStorefrontClient({
         mode: process.env.NODE_ENV,
         getLoadContext: () => ({session, storefront, env}),
       });
-  
+
       const response = await handleRequest(request);
-  
+
       if (response.status === 404) {
         /**
          * Check for redirects only when there's a 404 from the app.
@@ -55,64 +62,66 @@ const {storefront} = createStorefrontClient({
          */
         return storefrontRedirect({request, response, storefront});
       }
-  
+
       return response;
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error);
       return new Response('An unexpected error occurred', {status: 500});
     }
+  },
+};
+
+/**
+ * This is a custom session implementation for your Hydrogen shop.
+ * Feel free to customize it to your needs, add helper methods, or
+ * swap out the cookie-based implementation with something else!
+ */
+class HydrogenSession {
+  sessionStorage;
+  session;
+  constructor(sessionStorage, session) {
+    this.sessionStorage = sessionStorage;
+    this.session = session;
   }
-  /**
-   * This is a custom session implementation for your Hydrogen shop.
-   * Feel free to customize it to your needs, add helper methods, or
-   * swap out the cookie-based implementation with something else!
-   */
-  class HydrogenSession {
-    sessionStorage;
-    session;
-    constructor(sessionStorage, session) {
-      this.sessionStorage = sessionStorage;
-      this.session = session;
-    }
 
-    static async init(request, secrets) {
-      const storage = createCookieSessionStorage({
-        cookie: {
-          name: 'session',
-          httpOnly: true,
-          path: '/',
-          sameSite: 'lax',
-          secrets,
-        },
-      });
+  static async init(request, secrets) {
+    const storage = createCookieSessionStorage({
+      cookie: {
+        name: 'session',
+        httpOnly: true,
+        path: '/',
+        sameSite: 'lax',
+        secrets,
+      },
+    });
 
-      const session = await storage.getSession(request.headers.get('Cookie'));
+    const session = await storage.getSession(request.headers.get('Cookie'));
 
-      return new this(storage, session);
-    }
-
-    get(key) {
-      return this.session.get(key);
-    }
-
-    destroy() {
-      return this.sessionStorage.destroySession(this.session);
-    }
-
-    flash(key, value) {
-      this.session.flash(key, value);
-    }
-
-    unset(key) {
-      this.session.unset(key);
-    }
-
-    set(key, value) {
-      this.session.set(key, value);
-    }
-
-    commit() {
-      return this.sessionStorage.commitSession(this.session);
-    }
+    return new this(storage, session);
   }
+
+  get(key) {
+    return this.session.get(key);
+  }
+
+  destroy() {
+    return this.sessionStorage.destroySession(this.session);
+  }
+
+  flash(key, value) {
+    this.session.flash(key, value);
+  }
+
+  unset(key) {
+    this.session.unset(key);
+  }
+
+  set(key, value) {
+    this.session.set(key, value);
+  }
+
+  commit() {
+    return this.sessionStorage.commitSession(this.session);
+  }
+}
