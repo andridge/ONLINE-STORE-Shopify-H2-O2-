b24ed0f77e0a002f0607eda794bf046f83a3023a
Andridge Munene <andridgemunene@MAC.local>
2023-04-13 16:33:53 +0300

    fCK

--- server.js
+++ server.js
@@ -20,22 +20,21 @@
        */
       const session = await HydrogenSession.init(request, [env.SESSION_SECRET]);
   
-      /**
-       * Create Hydrogen's Storefront client.
-       */
-      const {storefront} = createStorefrontClient({
-        cache,
-        waitUntil: (p) => executionContext.waitUntil(p),
-        buyerIp: getBuyerIp(request),
-        i18n: {language: 'EN', country: 'US'},
-        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-        storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
-        storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
-        storefrontId: env.PUBLIC_STOREFRONT_ID,
-        requestGroupId: request.headers.get('request-id'),
-      });
-  
+     /**
+ * Create Hydrogen's Storefront client.
+ */
+const {storefront} = createStorefrontClient({
+  cache,
+  waitUntil: (p) => executionContext.waitUntil(p),
+  buyerIp: getBuyerIp(request),
+  i18n: {language: 'EN', country: 'US'},
+  publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+  privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+  storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+  storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
+  storefrontId: env.PUBLIC_STOREFRONT_ID,
+  requestGroupId: request.headers && request.headers instanceof Headers && request.headers.get('request-id'),
+});
       /**
        * Create a Remix request handler and pass
        * Hydrogen's Storefront client to the loader context.
