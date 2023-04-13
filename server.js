10e6a84601d89be40d6acfb28cf6c34ba6a76c3a
Andridge Munene <andridgemunene@MAC.local>
2023-04-13 17:00:17 +0300

    fuckn

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Virtual entry point for the app
-import * as remixBuild from '@remix-run/server-runtime';
+import * as remixBuild from '@remix-run/dev/server-build';
 import {createStorefrontClient, storefrontRedirect} from '@shopify/hydrogen';
 import {
   createRequestHandler,
-  getBuyerIp,
+  getStorefrontHeaders,
   createCookieSessionStorage,
 } from '@shopify/remix-oxygen';
 
@@ -32,14 +32,13 @@ export default {
       const {storefront} = createStorefrontClient({
         cache,
         waitUntil,
-        buyerIp: getBuyerIp(request),
         i18n: {language: 'EN', country: 'US'},
         publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
         storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
         storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
         storefrontId: env.PUBLIC_STOREFRONT_ID,
-        requestGroupId: request.headers.get('request-id'),
+        storefrontHeaders: getStorefrontHeaders(request),
       });
 
       /**
