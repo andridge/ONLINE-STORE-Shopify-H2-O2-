029c73f89b35944f7df1e4bb377ef4457e780d79
Andridge Munene <andridgemunene@MAC.local>
2023-04-13 11:57:36 +0300

    fCK

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ import {
 /**
  * Export a fetch handler in module format.
  */
- async function fetch(request, env, executionContext) {
-  try {
-    // Your fetch function implementation goes here
-     /**
+export default {
+  async fetch(request, env, executionContext) {
+    try {
+      /**
        * Open a cache instance in the worker and a custom session instance.
        */
       if (!env?.SESSION_SECRET) {
@@ -64,12 +64,13 @@ import {
       }
 
       return response;
-  } catch (error) {
-    console.error(error);
-    return new Response('An unexpected error occurred', {status: 500});
-  }
-}
-
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return new Response('An unexpected error occurred', {status: 500});
+    }
+  },
+};
 
 /**
  * This is a custom session implementation for your Hydrogen shop.
@@ -124,4 +125,3 @@ class HydrogenSession {
     return this.sessionStorage.commitSession(this.session);
   }
 }
-export { fetch };
