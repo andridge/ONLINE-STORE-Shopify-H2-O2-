49b7746fefbfd73eb774f034d7c05a27753327e7
Andridge Munene <andridgemunene@MAC.local>
2023-04-13 16:31:29 +0300

    fCK

--- server.js
+++ server.js
@@ -10,63 +10,60 @@
   /**
    * Export a fetch handler in module format.
    */
-   export default function fetch(request, env, executionContext) {
-    return (async () => {
-      try {
-        // Only use caches if it's available (i.e. in the Service Worker context)
-        const cache = typeof caches !== 'undefined' ? await caches.open('hydrogen') : null;
+   export default async function fetch(request, env, executionContext) {
+    try {
+      // Only use caches if it's available (i.e. in the Service Worker context)
+      const cache = typeof caches !== 'undefined' ? await caches.open('hydrogen') : null;
   
+      /**
+       * Open a custom session instance.
+       */
+      const session = await HydrogenSession.init(request, [env.SESSION_SECRET]);
+  
+      /**
+       * Create Hydrogen's Storefront client.
+       */
+      const {storefront} = createStorefrontClient({
+        cache,
+        waitUntil: (p) => executionContext.waitUntil(p),
+        buyerIp: getBuyerIp(request),
+        i18n: {language: 'EN', country: 'US'},
+        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+        storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+        storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
+        storefrontId: env.PUBLIC_STOREFRONT_ID,
+        requestGroupId: request.headers.get('request-id'),
+      });
+  
+      /**
+       * Create a Remix request handler and pass
+       * Hydrogen's Storefront client to the loader context.
+       */
+      const handleRequest = createRequestHandler({
+        build: remixBuild,
+        mode: process.env.NODE_ENV,
+        getLoadContext: () => ({session, storefront, env}),
+      });
+  
+      const response = await handleRequest(request);
+  
+      if (response.status === 404) {
         /**
-         * Open a custom session instance.
+         * Check for redirects only when there's a 404 from the app.
+         * If the redirect doesn't exist, then `storefrontRedirect`
+         * will pass through the 404 response.
          */
-        const session = await HydrogenSession.init(request, [env.SESSION_SECRET]);
-  
-        /**
-         * Create Hydrogen's Storefront client.
-         */
-        const {storefront} = createStorefrontClient({
-          cache,
-          waitUntil: (p) => executionContext.waitUntil(p),
-          buyerIp: getBuyerIp(request),
-          i18n: {language: 'EN', country: 'US'},
-          publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-          privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-          storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
-          storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
-          storefrontId: env.PUBLIC_STOREFRONT_ID,
-          requestGroupId: request.headers.get('request-id'),
-        });
-  
-        /**
-         * Create a Remix request handler and pass
-         * Hydrogen's Storefront client to the loader context.
-         */
-        const handleRequest = createRequestHandler({
-          build: remixBuild,
-          mode: process.env.NODE_ENV,
-          getLoadContext: () => ({session, storefront, env}),
-        });
-  
-        const response = await handleRequest(request);
-  
-        if (response.status === 404) {
-          /**
-           * Check for redirects only when there's a 404 from the app.
-           * If the redirect doesn't exist, then `storefrontRedirect`
-           * will pass through the 404 response.
-           */
-          return storefrontRedirect({request, response, storefront});
-        }
-  
-        return response;
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        return new Response('An unexpected error occurred', {status: 500});
+        return storefrontRedirect({request, response, storefront});
       }
-    })();
-  };
   
+      return response;
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return new Response('An unexpected error occurred', {status: 500});
+    }
+  }
   /**
    * This is a custom session implementation for your Hydrogen shop.
    * Feel free to customize it to your needs, add helper methods, or
