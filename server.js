1473a08ba2be0e6284bd84490f6e7f936d588e42
Andridge Munene <andridgemunene@MAC.local>
2023-04-13 16:00:37 +0300

    fCK

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Virtual entry point for the app
-import * as remixBuild from '@remix-run/react';
+import * as remixBuild from '@remix-run/server-runtime';
 import {createStorefrontClient, storefrontRedirect} from '@shopify/hydrogen';
 import {
   createRequestHandler,
